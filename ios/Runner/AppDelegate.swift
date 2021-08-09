0e64f30ad05fe52c58343b3e131831be0f7908aa
2UN7 <1491859758@qq.com>
2021-08-09 18:16:43 +0800

    与原生通信

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,11 +3,36 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    
+    override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        GeneratedPluginRegistrant.register(with: self)
+        if let controller = window.rootViewController as? FlutterBinaryMessenger {
+            let batteryChannel = FlutterMethodChannel(name: "my.flutter.io/battery", binaryMessenger: controller)
+            batteryChannel.setMethodCallHandler { [weak self] call, result in
+                if call.method == "getBatteryLevel" {
+                    let batteryLevel = self?.getBatteryLevel() ?? -1
+                    if batteryLevel == -1 {
+                        result(FlutterError(code: "UNAVAILABLE", message: "电池信息不可用", details: nil))
+                    } else {
+                        result(batteryLevel)
+                    }
+                } else {
+                    result(FlutterMethodNotImplemented)
+                }
+            }
+        }
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+}
+
+extension AppDelegate {
+    private func getBatteryLevel() -> Int {
+        let device = UIDevice.current
+        device.isBatteryMonitoringEnabled = true
+        if (device.batteryState == .unknown) {
+            return -1
+        } else {
+            return Int(device.batteryLevel * 100)
+        }
+    }
 }
--- lib/pages/discover/page_discover.dart
+++ lib/pages/discover/page_discover.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class DiscoverPage extends StatefulWidget {
   const DiscoverPage({Key? key}) : super(key: key);
@@ -8,13 +9,44 @@ class DiscoverPage extends StatefulWidget {
 }
 
 class _DiscoverPageState extends State<DiscoverPage> {
+  static const platform = const MethodChannel('my.flutter.io/battery');
+  String _batteryLevel = '未知电量';
+
+  @override
+  void initState() {
+    super.initState();
+    _getBatteryLevel();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("发现")),
       body: Center(
-        child: Text("发现"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(_batteryLevel),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(onPressed: _getBatteryLevel, child: Text("获取电量"))
+          ],
+        ),
       ),
     );
+  }
+
+  Future<Null> _getBatteryLevel() async {
+    String batteryLevel;
+    try {
+      final int result = await platform.invokeMethod('getBatteryLevel');
+      batteryLevel = "当前电量: $result%";
+    } on PlatformException catch (e) {
+      batteryLevel = "获取电量失败: '${e.message}'.";
+    }
+    setState(() {
+      _batteryLevel = batteryLevel;
+    });
   }
 }
